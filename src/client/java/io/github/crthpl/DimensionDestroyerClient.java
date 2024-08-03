c5c36d136454fb59a0e7bbc185d5cd6cf48637ce
crthpl <theo@reinsberg.info>
2024-08-03 13:52:48 -0700

    initial; kciommti

---
+++ src/client/java/io/github/crthpl/DimensionDestroyerClient.java
@@ -0,0 +1,10 @@
+package io.github.crthpl;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DimensionDestroyerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
